87f305958cfa636383fe74680bc6493e5adccfdc
J <>
2026-06-19 22:41:42 +0800

    v1.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+export CLISH_CONSOLE_PROJECT_BANNER="
+    __  _______  ____  __  ____    ___   _____ __  __
+   /  |/  / __ \/ __ \/ / / / /   /   | / ___// / / /
+  / /|_/ / / / / / / / / / / /   / /| | \__ \/ /_/ / 
+ / /  / / /_/ / /_/ / /_/ / /___/ ___ |___/ / __  /  
+/_/  /_/\____/_____/\____/_____/_/  |_/____/_/ /_/                                            
+"
+
+export CLISH_CONSOLE_PROJECT_NAME="Modulash"
+export CLISH_CONSOLE_PROJECT_DESCRIPTION="The modular command-line toolkit for DevOps"
